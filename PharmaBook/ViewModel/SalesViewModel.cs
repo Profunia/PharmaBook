710b707132929bf8a13e17c91ad6d14153370438
md hasanujjaman <mdhasan.hasan6@gmail.com>
2017-11-01 20:53:13 +0530

    Bulk Upload Changes

--- PharmaBook/ViewModel/SalesViewModel.cs
+++ PharmaBook/ViewModel/SalesViewModel.cs
@@ -11,7 +11,7 @@ namespace PharmaBook.ViewModel
         public InvcMstrVmdl masterinvc { get; set; }
         public IEnumerable<InvcChildVmdl> invcchld { get; set; }
         public List<ProductViewModel> successlst { get; set; }
-        public List<ProductViewModel> duplictlst { get; set; }
+        public List<Duplicatelist> duplictlst { get; set; }
         public List<ProductError> producterrlst { get; set; }
     }
 }
